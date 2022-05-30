b3cea0bdfbb78df009efdcdb72912de9beda3452
Guilherme Pedrosa <gui.pedrosa@hotmail.com>
2022-05-30 10:38:13 -0300

    first commit

---
+++ src/components/Quiz.js
@@ -0,0 +1,14 @@
+import Question from './Question'
+
+const Quiz = () => {
+    return(
+        <div className="quiz">
+            <div>
+                <div ClassName="score">Question 1/8</div>
+                <Question />
+            </div>
+        </div>
+    );
+};
+
+export default Quiz;
